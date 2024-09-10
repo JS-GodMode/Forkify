61b98ee026a8ebbeafaeb584a75d895c05b43a2a
Anonav0 <swarnavokhanra@gmail.com>
2024-09-10 20:12:31 +0530

    fixing code : try 1

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const API_KEY = '7ca96015-bac2-4f8f-a457-eb654491b36b';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const MODAL_CLOSE_SEC = 2.5;
